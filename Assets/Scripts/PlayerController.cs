8139a2ac2a0662c9b02b2828a301dd9e7baf1ffe
vlmolnar <lora9@hotmail.com>
2020-02-01 13:18:41 +0000

    Added separate character movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Public variables
+    public float speed;
+    public int playerId;
+
+    //Private variables
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal, moveVertical;
+        if (playerId == 1)
+        {
+            moveHorizontal = Input.GetAxis("Horizontal1");
+            moveVertical = Input.GetAxis("Vertical1");
+        } else
+        {
+            moveHorizontal = Input.GetAxis("Horizontal2");
+            moveVertical = Input.GetAxis("Vertical2");
+        }
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+    }
+}
