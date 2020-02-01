aa416e7e6d30767f2e4f68f1aecc435d23e2f354
mf220 <mf220@st-andrews.ac.uk>
2020-02-01 20:24:42 +0000

    Water has a front to it

---
+++ Assets/WaterFrontRising.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterFrontRising : MonoBehaviour
+{
+
+    public GameObject water;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = water.transform.position;
+    }
+}
