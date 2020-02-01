0bc8d4111fc6a50fbbc25565c852f40dd36863c3
vlmolnar <lora9@hotmail.com>
2020-02-01 23:36:22 +0000

    Added Title screen scene with start and quit buttons

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Control : MonoBehaviour
+{
+    public string nextSceneName;
+    public void NextScene()
+    {
+        SceneManager.LoadScene(nextSceneName);
+        Debug.Log("Game start");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+    }
+}
