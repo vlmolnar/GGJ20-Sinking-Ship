b794b480ab3f902f4326347bc1c70cbc459e766a
vlmolnar <lora9@hotmail.com>
2020-02-02 16:42:45 +0000

    Added background music

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -3,9 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(AudioSource))]
 public class Control : MonoBehaviour
 {
     public string nextSceneName;
+    AudioSource audioData;
+
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+        audioData.Play(0);
+        Debug.Log("started");
+        DontDestroyOnLoad(this.gameObject);
+    }
     public void NextScene()
     {
         SceneManager.LoadScene(nextSceneName);
